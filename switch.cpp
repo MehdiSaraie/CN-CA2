ae243544d48dab8a3245817a12851ef6afd2e22f
soudabemhashemi <s.mhashemi.221@gmail.com>
2021-04-26 13:32:22 +0430

    make file

--- switch.cpp
+++ switch.cpp
@@ -31,58 +31,73 @@ int main(int argc, char* argv[]){
 		pp[i][1] = 0;
 
 	while(true) {
-		FD_ZERO(&readfds);
+		// FD_ZERO(&readfds);
 
-		FD_SET(main_pipe_read_end, &readfds);
-		max_sd = main_pipe_read_end;
-		for (i = 0; i < number_of_ports; i++){
-			sd = pp[i][1];
-			if (sd > 0)
-				FD_SET(sd, &readfds);
-			if (sd > max_sd)
-				max_sd = sd;
-		}
-		activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
-		cout << activity << endl;		
-		if ((activity < 0) && (errno!=EINTR)){
-			cout << ("select error\n");
-		}
+		// FD_SET(main_pipe_read_end, &readfds);
+		// max_sd = main_pipe_read_end;
+		// for (i = 0; i < number_of_ports; i++){
+		// 	sd = pp[i][1];
+		// 	if (sd > 0)
+		// 		FD_SET(sd, &readfds);
+		// 	if (sd > max_sd)
+		// 		max_sd = sd;
+		// }
+		// activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+		// if ((activity < 0) && (errno!=EINTR)){
+		// 	cout << ("select error\n");
+		// }
 
-		if (FD_ISSET(main_pipe_read_end, &readfds)){ //msg from main
-			memset(&buffer, 0, LENGTH);
-			read(main_pipe_read_end, buffer, LENGTH);
-			istringstream line(buffer);
-			string command;
-			line >> command;
-			int token;
-			vector<int> tokens;
-			line >> command;
-			while(line >> token)
-				tokens.push_back(token);
-			if (command == "Connect"){
-				//add row to lookup
-				string port_number = to_string(tokens[2]);
-				const char* myfifo = &(switch_number + "-" + port_number)[0]; //(e.g. 1-40);
-    			mkfifo(myfifo, 0666);
-				int fd = open(myfifo ,O_RDONLY); //must store fd in pp beside its port
-				close(fd);
-			}
-		}
+		// if (FD_ISSET(main_pipe_read_end, &readfds)){ //msg from main
+		// 	memset(&buffer, 0, LENGTH);
+		// 	read(main_pipe_read_end, buffer, LENGTH);
+		// 	istringstream line(buffer);
+		// 	string command;
+		// 	line >> command;
+		// 	int token;
+		// 	vector<int> tokens;
+		// 	line >> command;
+		// 	while(line >> token)
+		// 		tokens.push_back(token);
+		// 	if (command == "Connect"){
+		// 		//add row to lookup
+		// 		cout << "here\n";
+		// 		string port_number = to_string(tokens[2]);
+		// 		const char* myfifo = &(switch_number + "-" + port_number)[0]; //(e.g. 1-40);
+    	// 		mkfifo(myfifo, 0666);
+		// 		// int fd = open(myfifo ,O_RDONLY); //must store fd in pp beside its port
+		// 		// close(fd);
+		// 	}
+		// }
 
-		for (i = 0; i < number_of_ports; i++){ //msg from a system
-			sd = pp[i][1];
-			if (FD_ISSET(sd, &readfds)){
-				//forward frame
-				const char* myfifo = &(switch_number + "-" + to_string(i))[0];
-				cout << myfifo << endl;
-				int fd1 = open(myfifo ,O_RDONLY);
-				memset(&buffer, 0, LENGTH);
-				read(fd1, buffer, LENGTH); 
-				cout << buffer << endl;
-				close(fd1);
-			}
+		// for (i = 0; i < number_of_ports; i++){ //msg from a system
+		// 	sd = pp[i][1];
+		// 	if (FD_ISSET(sd, &readfds)){
+		// 		//forward frame
+		// 		const char* myfifo = &(switch_number + "-" + to_string(i))[0];
+		// 		cout << myfifo << endl;
+		// 		int fd1 = open(myfifo ,O_RDONLY);
+		// 		memset(&buffer, 0, LENGTH);
+		// 		read(fd1, buffer, LENGTH); 
+		// 		cout << buffer << endl;
+		// 		close(fd1);
+		// 	}
+		// }
+
+		memset(&buffer, 0, LENGTH);
+		read(main_pipe_read_end, buffer, LENGTH);
+		istringstream line(buffer);
+		string command;
+		line >> command;
+		int token;
+		vector<int> tokens;
+		while(line >> token)
+			tokens.push_back(token);
+        if(command == "Connect"){
+            string port_number = to_string(tokens[2]);
+			const char* myfifo = &(switch_number + "-" + port_number)[0]; //(e.g. 1-40);
+			mkfifo(myfifo, 0666);
 		}
-    }
+	}
     return 0;
 }
 
